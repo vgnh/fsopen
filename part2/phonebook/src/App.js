9f93223072bf3a98341314290ffb55ad3a000f4e
Vignesh Mani <vigneshm21@protonmail.com>
2021-07-14 19:44:22 +0530

    same as before

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -91,7 +91,7 @@ const App = () => {
         .catch(error => {
           console.log(error.response.data)
           setErrorStatus(true)
-          setNotificationMessage(`${error.message}`)
+          setNotificationMessage(`${error.response.data}`)
           setTimeout(() => {
             setNotificationMessage(null)
             setErrorStatus(false)
