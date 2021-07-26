0e5b5462cf89669a77d25eb2e744e67e4790ab0f
Vignesh Mani <vigneshm21@protonmail.com>
2021-07-26 22:31:02 +0530

    Completed till 4.2

---
+++ part4/blog-list-backend/app.js
@@ -0,0 +1,30 @@
+const config = require('./utils/config.js')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs.js')
+const middleware = require('./utils/middleware.js')
+const logger = require('./utils/logger.js')
+const mongoose = require('mongoose')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use(middleware.requestLogger)
+
+app.use('/', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
